7f657a8eb19e45079f8d5a2c305def5e1167768f
vi5hnuu <vishnu.12017539@lpu.in>
2022-12-19 21:51:31 +0530

    hosted

---
+++ src/component/DeleteConfirmation.js
@@ -0,0 +1,11 @@
+/*
+Insert The Positive Thoughts...
+Delete The Negative Thoughts...
+Enter A Prosperous Life...
+*/
+
+import classes from './DeleteConfirmation.module.css'
+
+function DeleteConfirmation(props) {
+
+}
--- src/component/TaskList.js
+++ src/component/TaskList.js
@@ -1,6 +1,6 @@
 import classes from './TaskList.module.css'
 import Task from './Task'
-import { useContext, useEffect } from 'react'
+import { useContext } from 'react'
 import TaskContext from '../Store/TaskContext'
 
 function TaskList() {
@@ -8,7 +8,7 @@ function TaskList() {
     const tskCtx = useContext(TaskContext)
 
     return <ul className={classes.task_list}>
-        {tskCtx.tasks.length == 0 && <p className={classes.no_task}>😊No Task Found😊</p>}
+        {tskCtx.tasks.length === 0 && <p className={classes.no_task}>😊No Task Found😊</p>}
         {tskCtx.tasks.map(task => {
             return <Task key={task.id} id={task.id} taskText={task.taskText} />
         })}
